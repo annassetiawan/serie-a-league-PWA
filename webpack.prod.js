d889d070f73f3a2bfa1b2e2e89f72be0bdb2c1eb
annassetiawan <setiawanannas123@gmail.com>
2020-08-05 18:14:44 +0700

    Fix Webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const CopyPlugin = require('copy-webpack-plugin');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
  
@@ -31,6 +32,23 @@ module.exports = merge(common, {
     plugins: [
      new MiniCssExtractPlugin({
          filename: "[name].css"
+     }),
+     new CopyPlugin({
+         patterns:[
+             {
+                from: 'src/service-worker.js',
+             },
+             {
+                from: 'src/push.js',
+             },
+             {
+                from: 'src/manifest.json',
+             },
+             {
+                from: 'src/assets/',
+                to:'assets'
+             },
+         ]
      })
  ]
  })
