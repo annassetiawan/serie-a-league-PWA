a2fad266c5f22f119d23409383466d2706bb96dd
annassetiawan <setiawanannas123@gmail.com>
2020-08-05 08:11:53 +0700

    webpack v2

--- webpack.common.js
+++ webpack.common.js
@@ -8,28 +8,54 @@ module.exports = {
        filename: "bundle.js"
    },
    module: {
-       rules: [
-           {
-               test: /\.css$/,
-               use: [
-                   {
-                       loader: "style-loader"
-                   },
-                   {
-                       loader: "css-loader"
-                   }
-               ]
-           }
-       ]
+    rules: [
+        
+        {
+         test: /\.html$/,
+         use: ['html-loader']
+        },
+        {
+         test: /\.(svg|png|jpg|gif)$/,
+         use: [
+         {
+             loader:'file-loader',
+             options:{
+                 name:'[name].[ext]',
+                 outputPath:'imgs'
+             }
+         },
+         {
+             loader: 'image-webpack-loader',
+         }
+     ]
+        }
+    ]
    },
    plugins: [
        new HtmlWebpackPlugin({
            template: "./src/index.html",
            filename: "index.html"
        }),
-       new HtmlWebpackPlugin({  // Also generate a test.html
+       new HtmlWebpackPlugin({  
         filename: 'nav.html',
         template: './src/nav.html'
-      })
+      }),
+       new HtmlWebpackPlugin({  
+        filename: '/pages/saved.html',
+        template: './src/pages/saved.html'
+      }),
+       new HtmlWebpackPlugin({  
+        filename: '/pages/schedules.html',
+        template: './src/pages/schedules.html'
+      }),
+       new HtmlWebpackPlugin({  
+        filename: '/pages/standings.html',
+        template: './src/pages/standings.html'
+      }),
+       new HtmlWebpackPlugin({  
+        filename: '/pages/teams.html',
+        template: './src/pages/teams.html'
+      }),
+       
    ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,35 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
  
 module.exports = merge(common, {
-   mode: "production",
-   module: {
-       rules: [
-           {
-               test: /\.js$/,
-               exclude: "/node_modules/",
-               use: [
-                   {
-                       loader: "babel-loader",
-                       options: {
-                           presets: ["@babel/preset-env"]
-                       }
-                   }
-               ]
-           }
-       ]
-   }
-})
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                 MiniCssExtractPlugin.loader, // extact css into files
+                "css-loader", //turn css into bundle js
+             ]
+            },
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+     new MiniCssExtractPlugin({
+         filename: "[name].css",
+     })
+ ]
+ })
